77581a123ab325bede51035940ea9dabca593cd2
Juman <juman.developer@gmail.com>
2024-02-13 16:05:50 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Juman Dhaher on 03/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
