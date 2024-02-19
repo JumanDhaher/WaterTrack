dba4a2febaae24851f3cf46ea9d450f107fdb0a5
Juman <juman.developer@gmail.com>
2024-02-19 11:51:36 +0300

    water view

--- Hydrate/Views/onBordaingViews/NeedWaterView.swift
+++ Hydrate/Views/onBordaingViews/NeedWaterView.swift
@@ -12,50 +12,67 @@ struct NeedWaterView: View {
     @State var liters: Double
 
     var body: some View {
-        ScrollView{
             VStack(alignment:.leading,spacing: 10) {
                 
-                Text("The needed Water intake")
-                    .font(.title2).fontWeight(.bold)
+                Spacer(minLength: 15)
                 
-                Text("Your body needs \(liters) liters of hydration,\nwhich is equvalent to \(cups) cups").foregroundColor(.gray).font(.footnote).multilineTextAlignment(.leading)
+                Text("The needed Water intake")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text("Your body needs \(liters) liters of hydration,\nwhich is equvalent to \(cups) cups")  
+                    .font(.body)
+                    .foregroundColor(Color.gray)
+                    .padding(.bottom, 25)
+                
+                
                 
                 HStack(spacing: 30){
                     ZStack{
                         RoundedRectangle(cornerRadius: 20 )
                             .foregroundColor(.gray.opacity(0.2))
-                            .frame(width: 120,
-                                height: 120)
+                            .frame(width: 160, height: 159)
+                        
                         VStack{
                             Image("CupImage")
-                            Text("\(cups, specifier: "%.2f")cups")
+                            HStack{
+                                Text("\(cups, specifier: "%.2f")")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                Text("cups")
+                                    .font(.custom("SF Pro", size: 17))   .foregroundColor(.gray)
+                            }
                         }
                     }
                     
                     ZStack{
                         RoundedRectangle(cornerRadius: 20 )
                             .foregroundColor(.gray.opacity(0.2))
-                            .frame(width: 120,
-                                height: 120)
+                            .frame(width: 160, height: 159)
                         VStack{
                             Image("LiterImage")
-                            Text("\(liters, specifier: "%.2f")L")
-                        }
-
+                            HStack{
+                                Text("\(liters, specifier: "%.2f")")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                Text("L")
+                                    .font(.custom("SF Pro", size: 17))   .foregroundColor(.gray)
+                            }                        }
+                        
                     }
                 }
+                
+                Spacer()
+                
                 NavigationLink(destination: NotificationView(liters: liters, cups: cups), label: {
-                    Text("Set Notification")  
+                    Text("Set Notification")
                         .frame(width: 358,height: 55)
                         .background(.accent)
                         .foregroundColor( .white )
                         .cornerRadius(12)
-                        .padding(.bottom, 30)
                 })
                 
                 
-            }.padding(.top, 50)
-        }
+            }.padding(.vertical, 30)
     }
 }
 
